a7f48d3b2d968c5b4cf7103f2ea6664b334742f5
Antonio Montalvo <montalvocode@yahoo.com>
2016-08-21 16:46:41 -0700

    add firebase file

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,9 @@
+ console.log('hello');
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC6omfRwWGIasRJnWtTtKkHIi2tGpSmjSw",
+    authDomain: "sentiment-analysis-60e30.firebaseapp.com",
+    databaseURL: "https://sentiment-analysis-60e30.firebaseio.com",
+    storageBucket: "sentiment-analysis-60e30.appspot.com",
+  };
+  firebase.initializeApp(config);
