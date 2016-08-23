a9bff5df97dce5d88d64eee061db247b07b76f14
andrewnaeve <andrewnaeve@gmail.com>
2016-08-23 00:14:41 -0700

    basic firebase user tracking

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,26 +1,32 @@
- // Initialize Firebase
- var config = {
-   apiKey: "AIzaSyC6omfRwWGIasRJnWtTtKkHIi2tGpSmjSw",
-   authDomain: "sentiment-analysis-60e30.firebaseapp.com",
-   databaseURL: "https://sentiment-analysis-60e30.firebaseio.com",
-   storageBucket: "sentiment-analysis-60e30.appspot.com",
- };
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCreVhYp6XPrgxSbsTVuLPQxiCyjVARxtQ",
+  authDomain: "voice-d4c8b.firebaseapp.com",
+  databaseURL: "https://voice-d4c8b.firebaseio.com",
+  storageBucket: "",
+};
 firebase.initializeApp(config);
 
-// Create a variable to reference the database.
-var database = firebase.database();
+var dataRef = firebase.database();
 
+clickCounter = 0;
 
-database.ref('SentimentScores').on("value", function(snapshot) {
-
-	console.log('THE SUM OF ALL THING');
-
-
-// If any errors are experienced, log them to console. 
-}, function (errorObject) {
-
-  	console.log("The read failed: " + errorObject.code);
-
+$("#submitButtonOrWhatever").on('click', function() {
+    dataRef.ref().on('value', function(snapshot) {
+        dataRef.ref('userObject'+clickCounter).set({
+          words: words,
+          scores: scores
+        });
+      };
+  clickCounter += 1;
+  $("submitButtonOrWhatever").val();
+  return false
 });
 
 
+// If any errors are experienced, log them to console.
+}, function (errorObject) {
+
+   console.log("The read failed: " + errorObject.code);
+
+});
