2797e21ec2a7bdde1ccae2e780acad6a15a5091b
Antonio Montalvo <montalvocode@yahoo.com>
2016-08-24 10:00:07 -0700

    user auth test2

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -20,7 +20,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     $(".text_process_button").on('click', function() {
 
         dataRef.ref().on('value', function(snapshot) {
-            dataRef.ref('users/'+uid).push({
+            dataRef.ref('users/'+uid).set({
               avg: avg,
               textString: textString,
             });
@@ -32,10 +32,10 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
-// firebase.auth().signInAnonymously().catch(function(error) {
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-// })
+firebase.auth().signInAnonymously().catch(function(error) {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+})
 
 
 dataRef.ref().on('value', function(snapshot) {
