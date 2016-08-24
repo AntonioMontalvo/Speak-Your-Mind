d0bb6d43c18e933a9d0bfdb876c43627e423a753
Antonio Montalvo <montalvocode@yahoo.com>
2016-08-24 09:50:29 -0700

    user auth error

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -9,10 +9,7 @@ firebase.initializeApp(config);
 
 var dataRef = firebase.database();
 
-firebase.auth().signInAnonymously().catch(function(error) {
-  var errorCode = error.code;
-  var errorMessage = error.message;
-})
+
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
@@ -34,6 +31,13 @@ firebase.auth().onAuthStateChanged(function(user) {
     });
   }
 });
+
+firebase.auth().signInAnonymously().catch(function(error) {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+})
+
+
 dataRef.ref().on('value', function(snapshot) {
   var a = snapshot.numChildren();
   dataRef.ref('users/')
