3569a9622774df8e0166f50ec3f88e01836ed72f
Drake Smith <drakesmithjr@gmail.com>
2016-08-26 04:03:27 -0700

    merged front end

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,38 +1,24 @@
 // Initialize Firebase
-<<<<<<< HEAD
-var config = {
-  apiKey: "AIzaSyCreVhYp6XPrgxSbsTVuLPQxiCyjVARxtQ",
-  authDomain: "voice-d4c8b.firebaseapp.com",
-  databaseURL: "https://voice-d4c8b.firebaseio.com",
-  storageBucket: "voice-d4c8b.appspot.com",
-};
-=======
   var config = {
     apiKey: "AIzaSyC6omfRwWGIasRJnWtTtKkHIi2tGpSmjSw",
     authDomain: "sentiment-analysis-60e30.firebaseapp.com",
     databaseURL: "https://sentiment-analysis-60e30.firebaseio.com",
     storageBucket: "sentiment-analysis-60e30.appspot.com",
   };
->>>>>>> frontend
+
 firebase.initializeApp(config);
 
 var dataRef = firebase.database();
 
-
-
-<<<<<<< HEAD
-=======
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     var isAnonymous = user.isAnonymous;
     var uid = user.uid;
     console.log(uid)
->>>>>>> frontend
 
     $(".text_process_button").on('click', function() {
 
-<<<<<<< HEAD
-=======
+
         dataRef.ref().on('value', function(snapshot) {
             dataRef.ref('users/'+uid).set({
               avg: avg,
@@ -57,7 +43,7 @@ dataRef.ref().on('value', function(snapshot) {
   var a = snapshot.numChildren();
   dataRef.ref('users/');
 });
->>>>>>> frontend
+
 
 var query = firebase.database().ref("users").orderByKey();
 query.once("value")
