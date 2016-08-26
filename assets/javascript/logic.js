4bd7418a9e61e66b6b53939eedff545bfa0e2bbd
Antonio Montalvo <montalvocode@yahoo.com>
2016-08-25 18:12:20 -0700

    loop3

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -7,7 +7,7 @@ var usersAvg = [];
 
 $(".text_process_button").on('click', function(){
     for (var i = 0; i < usersAvg.length; i++){
-    var totalMean = parseInt(usersAvg[i]/usersAvg.length);
+    var totalMean = parseFloat(usersAvg[i]/usersAvg.length);
   }
   console.log ('The mean is: ' + totalMean);
 });
