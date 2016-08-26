85177f1d248b8b1da344cce852b0bf5b102c4554
Antonio Montalvo <montalvocode@yahoo.com>
2016-08-26 15:57:32 -0700

    mean3

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -10,7 +10,7 @@ $("#submit").on('click', function(){
 function getSum(a, b) {
     return a + b;
 }
-console.log('The mean is: ' + usersAvg.reduce(getSum)/usersAvg.length);
+$('#average').append(usersAvg.reduce(getSum)/usersAvg.length);
 });
 
 
