bfca1b25015fb93cba6fa28222ed519eb6c53be9
andrewnaeve <andrewnaeve@gmail.com>
2016-08-26 00:04:44 -0700

    firefixed

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,10 +1,10 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyC6omfRwWGIasRJnWtTtKkHIi2tGpSmjSw",
-    authDomain: "sentiment-analysis-60e30.firebaseapp.com",
-    databaseURL: "https://sentiment-analysis-60e30.firebaseio.com",
-    storageBucket: "sentiment-analysis-60e30.appspot.com",
-  };
+var config = {
+  apiKey: "AIzaSyCreVhYp6XPrgxSbsTVuLPQxiCyjVARxtQ",
+  authDomain: "voice-d4c8b.firebaseapp.com",
+  databaseURL: "https://voice-d4c8b.firebaseio.com",
+  storageBucket: "voice-d4c8b.appspot.com",
+};
 firebase.initializeApp(config);
 
 var dataRef = firebase.database();
@@ -15,19 +15,19 @@ var dataRef = firebase.database();
 
 
 
-dataRef.ref().on('value', function(snapshot) {
-
-  var a = snapshot.numChildren();
-  dataRef.ref('users/');
-});
-
-var query = firebase.database().ref("users").orderByKey();
-query.once("value")
-  .then(function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-      var key = childSnapshot.key;
-      var childData = childSnapshot.val();
-      usersAvg.push(childData.avg);
-      console.log(childData.avg);
-  });
-});//iterates over database an pushes the average of every user to array usersAvg
+// dataRef.ref().on('value', function(snapshot) {
+//
+//   var a = snapshot.numChildren();
+//   dataRef.ref('users/');
+// });
+//
+// var query = firebase.database().ref("users").orderByKey();
+// query.once("value")
+//   .then(function(snapshot) {
+//     snapshot.forEach(function(childSnapshot) {
+//       var key = childSnapshot.key;
+//       var childData = childSnapshot.val();
+//       usersAvg.push(childData.avg);
+//       console.log(childData.avg);
+//   });
+// });//iterates over database an pushes the average of every user to array usersAvg
