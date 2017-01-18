41126086b89376d2fc0e8efd185310cc043dac8d
Drake Smith <drakesmithjr@gmail.com>
2017-01-17 23:44:20 -0800

    add color coordination for sent score result

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -64,11 +64,7 @@ $(".text_process_button").click(function(){//This is the Get Sentiment Scores bu
             //we access Sentiment Analysis Results and write the type and score
             //$("#sentimentScorePanel").html(result.type+" = "+result.score);
             var scorePercent = Math.round(Math.abs(result.score * 100));
-            $('#sentiment').html((result.type).charAt(0).toUpperCase() + (result.type.slice(1)))
-                .css({
-                    "font-weight": "bold",
-                    "color": sentimentColor(result.type)
-                });
+            $('#sentiment').html((result.type).charAt(0).toUpperCase() + (result.type.slice(1))).css("color", sentimentColor(result.type));
             $('#sentscore').html(scorePercent + "%.");
 
             // $("#sentimentScore").html(result.score);
